b5421c7f9982e23a22d3e0fe0b53a604b0387248
mathbike <mathbike8@gmail.com>
2022-11-22 13:05:51 -0500

    d

---
+++ newpy.sh
@@ -0,0 +1,9 @@
+echo "enter the name of the directory:" && read NAME
+git clone https://github.com/mathbike/newpy ~/$NAME
+rm -rf .git
+cd $NAME
+python3 -m venv .venv
+COMMANDS_TXT="z.1_commands.txt"
+echo -e "########## COMMANDS ##########\n" > $COMMANDS_TXT
+echo "pip freeze > requirements.txt" >> $COMMANDS_TXT
+code .
