1d59186ae02435e43d0b15d1ce7f6ec94255386f
mathbike <mathbike8@gmail.com>
2022-11-22 13:13:03 -0500

    d

--- newpy.sh
+++ newpy.sh
@@ -1,7 +1,7 @@
 echo "enter the name of the directory:" && read NAME
 git clone https://github.com/mathbike/newpy ~/$NAME
-rm -rf .git newpy.sh
 cd $NAME
+rm -rf .git newpy.sh
 python3 -m venv .venv
 COMMANDS_TXT="z.1_commands.txt"
 echo -e "########## COMMANDS ##########\n" > $COMMANDS_TXT
